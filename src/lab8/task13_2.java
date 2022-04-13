d392675e14569226bc13c0be58e94c4e4ad5bc76
IlyaPopov345 <boovox890@gmail.com>
2022-04-13 19:15:11 +0600

    перенес классы в папку lab8

--- src/lab8/task13_2.java
+++ src/lab8/task13_2.java
@@ -1,6 +1,7 @@
+package lab8;
 
 import java.util.Scanner;
-public class task132 {
+public class task13_2 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String a = s.nextLine();
--- src/lab8/task8.java
+++ src/lab8/task8.java
@@ -1,3 +1,5 @@
+package lab8;
+
 import java.util.Scanner;
 public class task8
 {
