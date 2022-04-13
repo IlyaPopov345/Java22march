522dbba50d70e2e96ddfbf9209393c75eb2556d1
IlyaPopov345 <boovox890@gmail.com>
2022-04-13 19:49:33 +0600

    Решил задачу 1 в папке lab8

---
+++ src/lab8/task1.java
@@ -0,0 +1,27 @@
+package lab8;
+
+import java.util.Scanner;
+
+public class task1 {
+
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String a = s.nextLine();
+        String[] arr = a.split(" ");
+        int i = s.nextInt();
+
+         if (i < 0 || i > arr.length)
+         {
+
+             System.out.print("Число A должно быть в интервале [0, размер массива)");
+
+         }
+else {
+
+             System.out.println("В массиве длиной " + arr.length);
+             System.out.println("На индексе " + i);
+             System.out.println("Находится элемент со значением " + arr[i]);
+         }
+    }
+}
