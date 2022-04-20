0c3bca09283b46fe2c2765de36fa676267e1bd2c
adm <boovox890@gmail.com>
2022-04-20 20:34:18 +0600

    дорешать 17

--- src/lab8/task17_2.java
+++ src/lab8/task17_2.java
@@ -2,24 +2,30 @@ package lab8;
 
 import java.util.Scanner;
 
-public class task17 {
+public class task17_2 {
+
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String a = s.nextLine();
         String[] arr = a.split(" ");
         String x;
-        String z ;
-        int i = 0;
-        x = arr[i];
-        arr[i] = arr[arr.length - 1];
+
+        int n = 5;
+        int i = 4;
+
+        x = arr[arr.length - 1];
         while (i < arr.length - 1) {
-            i++;
-            z = arr[i];
-            arr[i] = x;
-            x = z;
+            arr[n] = arr[i];
+            n--;
+            i--;
         }
+        arr[n] = x;
+
+
         for (int y = 0; y < arr.length; y++) {
             System.out.print(" " + arr[y]);
         }
     }
 }
+
+
