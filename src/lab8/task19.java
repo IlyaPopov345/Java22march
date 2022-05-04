cddcba363cd4607d15f7dbf66701eae7fbea5a21
PopovIR <boovox890@gmail.com>
2022-05-04 09:23:14 +0600

    сделал задние 19 в лаб8

--- src/lab8/task19.java
+++ src/lab8/task19.java
@@ -9,11 +9,16 @@ public class task19 {
         Scanner s = new Scanner(System.in);
         String a = s.nextLine();
         String[] arr = a.split(" ");
+        String c;
 
+        for (int i = 0; i < arr.length / 2; i++) {
+           c= arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = arr[i];
+            arr[i] = c;
 
-        for (int y = arr.length - 1; y >= 0; y--) {
+        }
+        for (int y = 0; y < arr.length; y++) {
             System.out.print(" " + arr[y]);
-
         }
     }
 }
