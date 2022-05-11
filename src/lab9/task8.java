1eac1eebdacf7a9dc312515690b81fe947ae3e30
PopovIR <boovox890@gmail.com>
2022-05-11 10:00:06 +0600

    разобраться с уловниями в 8

--- src/lab9/task8.java
+++ src/lab9/task8.java
@@ -15,20 +15,23 @@ public class task8 {
             num[i] = Integer.parseInt(arr[i]);
         }
         for (int y = 0; y < arr.length - 1; y++) {
-            if (num[y + 1] < num[y]) {
-                System.out.println(" Значения отсортированы по возрастанию ");
+
+            if  (num[y]< num[y+1]) {
+                System.out.println(" Значения отсортированы по возрастанию");
                 return;
             }
-
-
-                else if (num[y + 1] < num[y]) {
+         if (num[y] > num[y+1]) {
 
                     System.out.println(" Элемент со значением " + num[y + 1] + " на индексе  " + (y + 1) + " нарушает закономерность ");
                     return;
                 }
+
+
+           }
+
             }
         }
-    }
+
 
 
 
