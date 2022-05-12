ea08228a7c9f97aea9b66f931be0d4bf79740080
PopovIR <boovox890@gmail.com>
2022-05-12 12:47:11 +0600

    Сделано задние 5 разобратся с условиямим в начале

--- src/lab9/task5.java
+++ src/lab9/task5.java
@@ -3,32 +3,58 @@ package lab9;
 import java.util.Scanner;
 
 public class task5 {
-    public class task2 {
-        public static void main(String[] args) {
-            Scanner s = new Scanner(System.in);
-            System.out.println(" Введите левый массив ");
-            String str = s.nextLine();
-            String[] left = str.split(" ");
-            int[] num = new int[left.length];
-            System.out.println(" Введите правый массив массив ");
-            Scanner b = new Scanner(System.in);
-            String str2 = b.nextLine();
-            String[] right = str2.split(" ");
-            int[] num2 = new int[right.length];
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(" Введите fragments ");
+        String str = s.nextLine();
+        String[] fra = str.split(" ");
+
+        System.out.println(" Введите indexes ");
+        Scanner b = new Scanner(System.in);
+        String str2 = b.nextLine();
+        String[] ind = str2.split(" ");
+        int[] num = new int[ind.length];
+
+        if (fra.length == 0) {
+
+            System.out.print(" Отсутствуют данные о фрагментах ");
+            return;
+        }
+     else if    (num.length == 0) {
+            System.out.print(" Отсутствуют данные о фрагментах ");
+            return;
+        }
+        for (int i = 0; i < num.length; i++) {
+            num[i] = Integer.parseInt(ind[i]);
+        }
+
+
+        for (int y = 0; y < num.length; y++) {
+
+            if (num[y] >= fra.length) {
+
+                System.out.println(" Фрагмент на индексе " + num[y] + " отсутствует ");
+            } else {
+                System.out.println(" Значение элемента на индексе " + num[y] + " это " + fra[num[y]]);
+            }
 
 
         }
     }
-
-
-
-
-
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
