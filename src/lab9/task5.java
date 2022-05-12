7d770caa3e5fa353381dd4296bd6356e28dac97b
PopovIR <boovox890@gmail.com>
2022-05-12 10:30:02 +0600

    создано задание 5

---
+++ src/lab9/task5.java
@@ -0,0 +1,35 @@
+package lab9;
+
+import java.util.Scanner;
+
+public class task5 {
+    public class task2 {
+        public static void main(String[] args) {
+            Scanner s = new Scanner(System.in);
+            System.out.println(" Введите левый массив ");
+            String str = s.nextLine();
+            String[] left = str.split(" ");
+            int[] num = new int[left.length];
+            System.out.println(" Введите правый массив массив ");
+            Scanner b = new Scanner(System.in);
+            String str2 = b.nextLine();
+            String[] right = str2.split(" ");
+            int[] num2 = new int[right.length];
+
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
