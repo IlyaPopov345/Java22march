bd3c7a44c8fe01b07bccd76508593dade2787c7d
PopovIR <boovox890@gmail.com>
2022-05-16 10:55:18 +0600

    начал задание 6

--- src/lab9/task6.java
+++ src/lab9/task6.java
@@ -8,18 +8,13 @@ public class task6 {
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
         String[] arr = str.split(";");
-        int[] num = new int[arr.length];
 
 
-        for (int i = 0; i < num.length; i++) {
 
-            num[i] = Integer.parseInt(arr[i]);
 
-        }
         for (int y = 0; y < arr.length; y++) {
-
-
-            System.out.println(num[y] = '\u0023'); // как сделать зависимость смвола # от
+            System.out.println( arr[y] = " #");
+            // как сделать зависимость смвола # от
             // значения соответсвующего элемента в массиве
         }
     }
