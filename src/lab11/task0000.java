f33f4b66376f302469bd7f5acc8d99934e7a2d05
PopovIR <boovox890@gmail.com>
2022-06-16 17:26:06 +0600

    *

--- src/lab11/task0000.java
+++ src/lab11/task0000.java
@@ -3,7 +3,7 @@ package lab11;
 import java.util.Scanner;
 
 public class task0000 {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
---
+++ src/lab11/task0001.java
@@ -0,0 +1,32 @@
+package lab11;
+
+import java.lang.IllegalArgumentException;
+
+import java.util.Scanner;
+
+public class task0001 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        double result = 0;
+
+            double a = s.nextInt();
+            double b = s.nextInt();
+
+
+
+        try {
+            if (b<=0 ) {
+                result = a * Math.sqrt(-7 * b);
+                System.out.println(result);
+            }
+        }
+
+        catch (IllegalArgumentException v){
+            System.out.print( "Подкоренное выражение должно быть неотрицательно");
+        }
+
+    }
+}
+
+
