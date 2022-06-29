2b6207b7c8b226309c70629377b7e63552533228
IlyaPopov345 <boovox890@gmail.com>
2022-06-29 20:58:32 +0600

    dobavilis zadania

--- src/lab16/task3.java
+++ src/lab16/task3.java
@@ -9,27 +9,44 @@ public class task3 {
 
 
     public static void main(String[] args) {
-        File file = new File("resourser/task8733/test2.txt");
+        File file = new File("resourser/task8733/test9.txt");
 
         try {
             Scanner s = new Scanner(file);
             double sum = 0;
             double x = 0;
-            int count = 0;  // НЕ ЗНАЮ КАК СДЛЕАТЬ ЧТОБЫ  ПЕРВОЕ ЧИСЛО НЕ УЧАСТВОВАЛО  В РАСЧЕТЕ
+            int count = 0;
+            int count2 = 0;
 // НЕ ЗНАЮ КАК СДЕЛАТЬ /Некорректный объем данных\
             try {
 
+                String line = s.nextLine();
+                x = Integer.parseInt(line);
+
                 while (s.hasNext()) {
-                    String line = s.nextLine();
+
+                    line = s.nextLine();
                     int i = Integer.parseInt(line);
                     if (i <= 20) {
                         sum = sum + i;
+                        count++;
                     }
 
-                    count++;
+                    if (count2 == x) {
+                        System.out.println("Некорректный объем данных");
+                        break;
+
+                    }
+                    count2++;
                 }
-                x = ((sum) / count);
-                System.out.println(x);
+                if (count2 ==  x) {
+                    x = ((sum) / count);
+                    System.out.println(x);
+                    System.out.println("Всего строк:" + count2);
+                }
+               else   {
+                    System.out.println("Некорректный объем данных2");
+                }
             } catch (NumberFormatException p) {
                 System.out.println("Не удается считать число");
             }
